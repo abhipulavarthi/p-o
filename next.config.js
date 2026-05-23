80491c33db0795c33749be11c8e6d8f10e0bc14c
Abhinav <abhinav.pulavarthi06@gmail.com>
2026-05-24 00:33:12 +0530

    script modified to add css

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
+
     images: {
         unoptimized: true,
     },
+
     basePath: '/p-o',
     assetPrefix: '/p-o/',
 }
