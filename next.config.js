a8d73caf63cfa1d9da539082b304c29f347e1139
Abhinav <abhinav.pulavarthi06@gmail.com>
2026-05-24 00:17:49 +0530

    added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
     basePath: '/p-o',
-};
+    assetPrefix: '/p-o/',
+}
 
-module.exports = nextConfig;
-
+module.exports = nextConfig
